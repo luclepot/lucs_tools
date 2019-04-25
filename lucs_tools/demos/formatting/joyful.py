177cf700d3018573987f93a1c52c4a0b08864b1e
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-04-25 21:13:13 +0200

    added ascii formatter, images

--- lucs_tools/demos/formatting/joyful.py
+++ lucs_tools/demos/formatting/joyful.py
@@ -1,22 +1,23 @@
 import lucs_tools
+import numpy as np
 
-# get an input string (single string, no returns!)
-testfile = "{}/data/formatting/porto.txt".format(lucs_tools.MODULE_PATH)
-input_string = open(testfile).read()
-input_string.replace('\n', '')
-print('INPUT:')
-print(input_string)
-print()
 
 # call lucs_tools.formatting.joyful.joy_text to plot the string
-lucs_tools.formatting.joyful.joy_text(
-    input_string,
-    90,
-    fontsize=5.5,
-    weight='light',
-    subaspect=2.5,
-    factor=3.
-)
+# lucs_tools.formatting.joyful.text(
+#     input_string,
+#     90,
+#     fontsize=5.5,
+#     weight='light',
+#     factor=3.,
+#     write_lines=True,
+# )
 
-# call lucs_tools.formatting.joyful.joy_plot to plot a standard joyd cover
-# lucs_tools.formatting.joyful.joy_plot(10, 100)
+# standard joy division cover recreation:
+lucs_tools.formatting.joyful.lines(90, size=(400,1000))
+
+# lucs_tools.formatting.joyful.joy_text_new(text, size=(500,600), spacing=4., fontsize=9., write_lines=False, weight='normal')
+
+
+# ascii formatting; returns a loooong string you can view, from an original seed string (cuts/loops if needed)
+# text = """it was a good run. A new coach to soccer that coaching rec ed would realize if you're down 3-0 in the first 25 mins of the game, you need to change your strategy. Scolari and the rest of the coaching staff including perreira will never be apart of the brasilian futbol federation again. There were so many players to choose. He chose jo and fred one of the two most lazy ass strikers to be his "offense". Then I realize that  pato is 24 why not play him. How about fabiano or even coutinho or damiao. It's crazy, he made a team centered around neymar. Kaka ronaldinho or robinho should of made the team. Congrats on germany, if argentina goes to the final they better not fuck up man. Because that will be a nightmare if the argentines win in brasil. GO NEUR AND SHURLLE"""
+# print(lucs_tools.formatting.joyful.ascii(text, 300, 60, 0, 2))
