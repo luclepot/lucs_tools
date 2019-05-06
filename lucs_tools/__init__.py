6f5c49cc2765c376187145fa3105c29e78348eb3
Luc Le Pottier <luc.tomas.le.pottier@cern.ch>
2019-05-06 19:17:47 +0200

    some small renames, added some general tools

--- lucs_tools/__init__.py
+++ lucs_tools/__init__.py
@@ -2,6 +2,7 @@ import lucs_tools.command_line
 import lucs_tools.formatting
 import lucs_tools.internet
 import lucs_tools.logs
+import lucs_tools.general
 import os 
 
 MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
